e117a6577de13249bedeb3ee56a224a91952453f
Chun-Hung <32384566+r06942072@users.noreply.github.com>
2019-04-20 01:01:55 -0400

    ssss

---
+++ test_tweet/Readme.md
@@ -0,0 +1,10 @@
+# Note 
+## Features:
+ 
+***
+## Ref
+*An Introduction to jQuery  
+https://www.digitalocean.com/community/tutorials/an-introduction-to-jquery
+
+
+
---
+++ test_tweet/src/main.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class TweetBox extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="card bg-light">
+                <div className="card-body text-right">
+                    <textarea className="form-control"></textarea>
+                    <br />
+                    <button className="btn btn-primary">Tweet</button>
+                </div>
+            </div>
+        );
+    }
+};
+
+/*
+takes two arguments: 1. React component, 2. DOM element
+renders the TweetBox UI inside <div id="container">
+*/
+ReactDOM.render(
+    <TweetBox />,
+    document.getElementById("container")
+);
